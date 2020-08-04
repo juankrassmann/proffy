1b49c8ffd9e3a5488c43c552ae5b1cc21ec78a30
Juan Felipe Krassmann <juanfkrassmann@gmail.com>
2020-08-04 16:49:43 -0300

    'first'

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Landing from './pages/landing'
+
+import './assets/styles/global.css';
+
+// JSX: javascript + xml (trabalhar com html/xml dentro do javascript)
+
+function App() {
+  return (
+    <Landing />
+  );
+}
+
+export default App;
