6c88032990034a0cc286ac04a187a14f55cf8bdb
Juan Felipe Krassmann <juanfkrassmann@gmail.com>
2020-08-05 16:47:26 -0300

    landing page implementation

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -4,12 +4,13 @@ import landingImg from '../../assets/images/landing.svg';
 import studyIcon from '../../assets/images/icons/study.svg';
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+import './style.css';
 
 function Landing() {
   return (
     <div id="page-landing">
         <div id="page-landing-content" className="container">
-          <div id="logo-container">
+          <div className="logo-container">
             <img src={logoImg} alt="Proffy" />
             <h2>Sua plataforma de estudos online.</h2>
           </div>
